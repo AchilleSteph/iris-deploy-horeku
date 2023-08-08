4f4da399cb3d27a49599785ac5a4ec60e2d28714
Achille Stephane <hyefouais@yahoo.com>
2023-08-08 16:10:52 -0400

    deploy model on Heroku

--- iris/app.py
+++ iris/app.py
@@ -17,7 +17,7 @@ def submit():
         petal_length = request.form["petallength"]
         petal_width = request.form["petalwidth"]
 
-        # data_entered = "Voici les données que vous avez entrées: " + sepal_length + ", " + sepal_width + ", " + petal_length + ", " + petal_width + ". Ce qui correspond a la variEtE : "
+
 
         class_pred = main.iris_prediction(float(sepal_length), 
                                           float(sepal_width), 
@@ -25,12 +25,13 @@ def submit():
                                           float(petal_width)
                                           )
         
-        pred_result = "Voici dans l'ordre les données que vous avez entrées: " + sepal_length + ", " + sepal_width + ", " + petal_length + ", " + petal_width + ". Ce qui correspond a la variété : " + class_pred
+        user_request = "Voici dans l'ordre les données que vous avez entrées: " + sepal_length + ", " + sepal_width + ", " + petal_length + ", " + petal_width  
+        pred_result = "Ce qui correspond a la variété : " + class_pred
 
-        
-    # take date from python file to html    
-    #return render_template("submit-page.html", sl=sepal_length, sw=sepal_width, pl=petal_length, pw=petal_width)
-    return render_template("submit-page.html", pred = pred_result)
+      
+      
+    
+    return render_template("submit-page.html", request = user_request, pred = pred_result)
     
 
 if __name__== "__main__":
