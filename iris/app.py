abc6f6f24f60fae190722b7a44f5da10b58115cc
Achille Stephane <hyefouais@yahoo.com>
2023-08-07 21:44:55 -0400

    deploy model that accepts float numbers

--- iris/app.py
+++ iris/app.py
@@ -17,17 +17,20 @@ def submit():
         petal_length = request.form["petallength"]
         petal_width = request.form["petalwidth"]
 
-        data_entered = "Voici les données que vous avez entrées: " + sepal_length + ", " + sepal_width + ", " + petal_length + ", " + petal_width + ". Ce qui correspond a la variEtE : "
+        # data_entered = "Voici les données que vous avez entrées: " + sepal_length + ", " + sepal_width + ", " + petal_length + ", " + petal_width + ". Ce qui correspond a la variEtE : "
 
-        class_pred = main.iris_prediction(int(sepal_length), 
-                                          int(sepal_width), 
-                                          int(petal_length), 
-                                          int(petal_width)
+        class_pred = main.iris_prediction(float(sepal_length), 
+                                          float(sepal_width), 
+                                          float(petal_length), 
+                                          float(petal_width)
                                           )
+        
+        pred_result = "Voici dans l'ordre les données que vous avez entrées: " + sepal_length + ", " + sepal_width + ", " + petal_length + ", " + petal_width + ". Ce qui correspond a la variété : " + class_pred
+
         
     # take date from python file to html    
     #return render_template("submit-page.html", sl=sepal_length, sw=sepal_width, pl=petal_length, pw=petal_width)
-    return render_template("submit-page.html", user_request = data_entered, pred = class_pred)
+    return render_template("submit-page.html", pred = pred_result)
     
 
 if __name__== "__main__":
